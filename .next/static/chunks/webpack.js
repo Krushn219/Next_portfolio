7515d300d796a918142dd667770e957c8fcb0948
Krushn219 <krushn.219@gmail.com>
2023-07-11 11:09:09 +0530

    change title

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a391e244284d233f"; }
+/******/ 		__webpack_require__.h = function() { return "0b64d799a78f8d09"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
