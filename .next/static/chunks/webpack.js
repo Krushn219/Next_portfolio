ea1bac75d2b0eaff2950ab2c53108b7c647dbaa9
Krushn219 <krushn.219@gmail.com>
2023-07-11 11:04:51 +0530

    fevicon change

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6ee701efb83c6d92"; }
+/******/ 		__webpack_require__.h = function() { return "a391e244284d233f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
